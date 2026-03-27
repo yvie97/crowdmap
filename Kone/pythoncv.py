e99c0af23d94930eeee8bfc34e3f71c8efcdbe90
Kone <huangshiyong0717@gmail.com>
2026-03-27 00:34:36 -0700

    sleep and jump some frames

--- Kone/pythoncv.py
+++ Kone/pythoncv.py
@@ -1,5 +1,5 @@
-"""
-"""
+import json
+import time
 import threading
 import cv2
 import uvicorn
@@ -9,42 +9,25 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from detector import detect_people   
 
-# ══════════════════════════════════════════════════════════════
 
-# ══════════════════════════════════════════════════════════════
-SOURCES = [
-    {
-        "area_id": "area_225_2f_1",
-        "video":   "demo_video01.MOV",
-        "roi":     (0, 0, 6000, 6000),
-    },
-        {
-        "area_id": "area_225_2f_2",
-        "video":   "demo_video02.MOV",
-        "roi":     (0, 0, 6000, 6000),
-     },
-        {
-        "area_id": "area_225_2f_3",
-        "video":   "demo_video02.MOV",
-        "roi":     (0, 0, 6000, 6000),
-     },
-        {
-        "area_id": "area_225_2f_4",
-        # "video":   "demo_video04.MOV",
-        "video":   0,           
-        "roi":     (0, 000, 6000, 6000),
-    }
-    # {
-    #     "area_id": "area_225_2f_4",
-    #     "video":   0,          
-    #     "roi":     (100, 100, 1820, 980),
-    # },
-]
-# ══════════════════════════════════════════════════════════════
+CONFIG_PATH = "cameras_config.json"
+PROCESS_EVERY_N_FRAMES = 5          # run per 5 frames
+
+# get info of scource
+def _load_sources(path: str) -> list[dict]:
+    with open(path, "r", encoding="utf-8") as f:
+        sources = json.load(f)
+
+    for src in sources:
+        src["roi"] = tuple(src["roi"])
+    return sources
+
+SOURCES = _load_sources(CONFIG_PATH)
 
 
 
-# ：{ area_id: {"count": int, "latest_frame": bytes | None} }
+
+# { area_id: {"count": int, "latest_frame": bytes | None} }
 _state: dict[str, dict] = {
     src["area_id"]: {"count": 0, "latest_frame": None}
     for src in SOURCES
@@ -57,6 +40,8 @@ def _run_source(src: dict):
     area_id = src["area_id"]
     roi     = src["roi"]
     video   = src["video"]
+    frame_idx = 0             
+
 
     cap = cv2.VideoCapture(video)
 
@@ -72,6 +57,10 @@ def _run_source(src: dict):
                
                 cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
                 continue
+
+        frame_idx += 1                                      
+        if frame_idx % PROCESS_EVERY_N_FRAMES != 0:        
+            continue
 
         count, annotated = detect_people(frame, roi)
 
@@ -91,8 +80,9 @@ def _frame_generator(area_id: str):
             yield (b"--frame\r\n"
                    b"Content-Type: image/jpeg\r\n\r\n" + jpeg + b"\r\n")
 
+        time.sleep(0.03)          # sleep when no new frame comes in
 
-# ── ─────────────────────────────────────────────
+
 app = FastAPI()
 
 app.add_middleware(
